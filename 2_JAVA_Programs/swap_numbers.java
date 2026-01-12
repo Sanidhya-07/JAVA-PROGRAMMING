b035e979c265bf8f4433c15cea27c20ecd12f56b
Sanidhya Naik <sanidhya.dev7@gmail.com>
2026-01-12 15:48:04 +0530

    swap numbers

---
+++ 2_JAVA_Programs/swap_numbers.java
@@ -0,0 +1,15 @@
+public class swap_numbers {
+    public static void main(String[] args) {
+        int num1=45;
+        int num2=98;
+
+        int temp;
+
+        temp=num1;
+        num1=num2;
+        num2=temp;
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
