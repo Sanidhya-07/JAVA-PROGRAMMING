0f3dce2252578dd408e802dcd6b3e9af19ec6c28
Sanidhya Naik <sanidhya.dev7@gmail.com>
2026-01-13 12:39:16 +0530

    java programs

---
+++ 2_JAVA_Programs/product_of_float.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class product_of_float {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter your first decimal number: ");
+        float num1=input.nextFloat();
+
+        System.out.print("Enter your second decimal number: ");
+        float num2=input.nextFloat();
+    }
+}
