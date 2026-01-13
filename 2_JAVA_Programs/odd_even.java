90c98a57c5a4b8302ff235768e92ad4da2ce23c8
Sanidhya Naik <sanidhya.dev7@gmail.com>
2026-01-13 18:49:01 +0530

    Odd Even

---
+++ 2_JAVA_Programs/odd_even.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class odd_even {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your number : ");
+        int number = input.nextInt();
+
+        if (number%2==0){
+            System.out.println("Number is EVEN");
+        }
+        else{
+            System.out.println("Number is ODD");
+        }
+
+        input.close();
+        
+    }
+    
+}
