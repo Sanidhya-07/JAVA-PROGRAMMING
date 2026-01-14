6ed442343bda60a6c71d8f3462685be5033c4dd1
Sanidhya Naik <sanidhya.dev7@gmail.com>
2026-01-14 19:30:48 +0530

    Factorial of number

---
+++ 2_JAVA_Programs/factorial_of_number.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class factorial_of_number {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your number : ");
+        int num = input.nextInt();
+
+        int result = factorial(num);
+        System.out.println("Factorial of " + num + " is : " + result);
+
+        input.close();
+    }
+
+    public static int factorial(int num) {
+        int i = 1;
+        int fact = 1;
+
+        if (num == 1 || num == 0) {
+            return 1;
+        } 
+        else{
+
+            while (i <= num) {
+                fact = fact * i;
+                i++;
+            }
+        }
+
+        return fact;
+    }
+
+}
