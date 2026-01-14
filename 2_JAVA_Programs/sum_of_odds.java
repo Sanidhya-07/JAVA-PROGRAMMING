b472d29ad43bf18fc44587c88dbf6b30ad7c5059
Sanidhya Naik <sanidhya.dev7@gmail.com>
2026-01-14 19:03:47 +0530

    Odd Sum

---
+++ 2_JAVA_Programs/sum_of_odds.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class sum_of_odds {
+
+    
+    public static void Odds_Sum(int last){
+        int i = 0 ;
+        int sum = 0;
+
+        while(i <= last){
+            if(i % 2 == 1){
+                System.out.print(i + "\t");
+                sum += i;
+            }
+            i++;
+        }
+        System.out.println("\nSum of all these ODD numbers is : " + sum);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter last number of range : ");
+        int lastNumber = input.nextInt();
+
+        Odds_Sum(lastNumber);
+
+        input.close();
+
+    }
+
+    
+}
