95a85a69f1ba5938e4fe46c8868f500ebd42ada7
Sanidhya Naik <sanidhya.dev7@gmail.com>
2026-01-20 15:42:03 +0530

    Java programs

---
+++ 2_JAVA_Programs/_01_swap_numbers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class _01_swap_numbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first number : ");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter second number : ");
+        int num2 = input.nextInt();
+
+        System.out.println("Before Swapping :");
+        System.out.println("first number : " + num1);
+        System.out.println("second number : " + num2);
+
+        int temp;
+
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+
+        System.out.println("\n\nAfter Swapping :");
+        System.out.println("first number : " + num1);
+        System.out.println("second number : " + num2);
+
+        input.close();
+
+    }
+}
