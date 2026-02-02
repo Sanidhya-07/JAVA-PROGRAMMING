129f25c4fbb5898c152038d85742a89e1e1acb6d
Sanidhya Naik <sanidhya.dev7@gmail.com>
2026-02-02 10:09:55 +0530

    Java Programs

--- 2_JAVA_Programs/_12_HCF_of_numbers.java
+++ 2_JAVA_Programs/_12_HCF_of_numbers.java
@@ -4,57 +4,27 @@ public class _12_HCF_of_numbers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter three numbers : ");
+        System.out.println("Enter your first number:");
         int num1 = input.nextInt();
+
+        System.out.println("Enter your second number:");
         int num2 = input.nextInt();
-        int num3 = input.nextInt();
 
-        System.out.println("Your numbers are : " + num1 + "," + num2 + "," + num3);
-
-        int result = HCF(num1, num2, num3);
-
-        System.out.println("HCF of given numbers is : " + result);
-
+        int gcd = gcdOfNumbers(num1, num2);
+        System.out.printf("GCD of %d and %d is %d.", num1, num2, gcd);
         input.close();
 
     }
 
-    public static int leastnumber(int first, int second, int third) {
+    public static int gcdOfNumbers(int num1, int num2) {
 
-        int least;
+        while (num2 != 0) {
+            int temp = num2;
 
-        if (first < second) {
-            if (first < third) {
-                least = first;
-            } else {
-                least = third;
-            }
-        } else {
-            if (second < third) {
-                least = second;
-            } else {
-                least = third;
-            }
+            num2 = num1 % num2;
+            num1 = temp;
         }
-        return least;
+        return num1;
     }
 
-    public static int HCF(int num1, int num2, int num3) {
-
-        int leastNum = leastnumber(num1, num2, num3);
-
-        int HCF = 1;
-
-        int i = 1;
-        while (i <= leastNum) {
-
-            if ((num1 % i == 0) && (num2 % i == 0) && (num3 % i == 0)) {
-                HCF = i;
-            }
-            i++;
-        }
-
-        return HCF;
-
-    }
 }
--- 2_JAVA_Programs/_15_fibonacci_series.java
+++ 2_JAVA_Programs/_15_fibonacci_series.java
@@ -1,35 +1,31 @@
 import java.util.Scanner;
 
-public class _15_fibonacci_series {
+public class _15_fibonacci_series{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the count of fibonacci elements: ");
-        int count = input.nextInt();
-        fibo_series(count);
+        System.out.println("Enter your number of fibonacchi element:");
+        int limit = input.nextInt();
+
+        fibonacchi(limit);
 
         input.close();
+
     }
 
-    public static void fibo_series(int count) {
+    public static void fibonacchi(int num) {
 
-        int first = 0;
-        int second = 1;
-        int third;
+        int firstNum = 0;
+        int secondNum = 1;
+        int nextNum = 0;
 
-        int i = 0;
-        while (i < count) {
-
-            System.out.print(first + " ");
-
-            third = first + second;
-            first = second;
-            second = third;
-
-            i++;
+        for (int i = 1; i <= num; i++) {
+            System.out.printf("%d\t", firstNum);
+            nextNum = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = nextNum;
 
         }
 
     }
-
 }
