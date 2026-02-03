9e7b44e6eccf6de3ade647c55335d1fd8c8f36dd
Sanidhya Naik <sanidhya.dev7@gmail.com>
2026-02-03 12:48:47 +0530

    Java Programs

---
+++ 2_JAVA_Programs/_25_reverse_array.java
@@ -0,0 +1,30 @@
+public class _25_reverse_array {
+
+    public static void main(String[] args) {
+        int[] userArray = _18_arrayInput.arrayelements();
+
+        System.out.println("Your array is : ");
+        _18_arrayInput.showArray(userArray);
+
+        reverseArray(userArray);
+
+        System.out.println("\nreverse of array is : ");
+        _18_arrayInput.showArray(userArray);
+
+    }
+
+    public static void reverseArray(int[] array) {
+
+        int j = array.length - 1;
+
+        for (int i = 0; i < (array.length / 2); i++) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+
+            j--;
+
+        }
+    }
+
+}
