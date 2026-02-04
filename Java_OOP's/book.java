ce47457fc0c067573f2f077c231f5ff02a6dda69
Sanidhya Naik <sanidhya.dev7@gmail.com>
2026-02-04 20:12:36 +0530

    Java Programs

---
+++ Java_OOP's/book.java
@@ -0,0 +1,58 @@
+
+public class book {
+    // static variable associated with class not with its objects
+    static int totalBooks;
+
+    // instance variable
+    String title;
+    String author;
+    int isbnNo;
+    boolean isBorrowed;
+
+    // parameterized constructor
+    book(String title, String author, int isbnNo) {
+        this.title = title;
+        this.author = author;
+        this.isbnNo = isbnNo;
+
+        totalBooks++;
+    }
+
+    // methods
+    void borrowBook() {
+        if (isBorrowed) {
+            System.out.println("Book is already borrowed.");
+        } else {
+            System.out.println("Enjoy your book");
+            isBorrowed = true;
+
+        }
+    }
+
+    void returnBook() {
+        if (isBorrowed) {
+            System.out.println("please drop a review.");
+            isBorrowed = false;
+        } else {
+            System.out.println("Book is already in labrary.");
+        }
+    }
+
+    public static int getTotalBooks() {
+        return totalBooks;
+    }
+
+    public static void main(String[] args) {
+
+        // object creation
+        book firstBook = new book("wings", "Sanidhya", 0001);
+        book secondBook = new book("Dogs", "Prem", 0002);
+
+        firstBook.borrowBook();
+        firstBook.borrowBook();
+        firstBook.returnBook();
+        firstBook.returnBook();
+
+        System.out.println(book.getTotalBooks());
+    }
+}
